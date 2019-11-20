bc45b4f9933f9fd09efc06768134a72539e88499
Randson <orandson@gmail.com>
2019-11-20 19:59:23 -0300

    create method to load all ids from vuforia associated with server key

--- lib/vufer/target.rb
+++ lib/vufer/target.rb
@@ -17,8 +17,26 @@ module Vufer
         signature = Vufer::Signature.generate("/targets/#{id}", nil, 'GET', time)
 
         res = Faraday.get("#{Vufer::BASE_URI}/targets/#{id}", {}, {
-          'Date': time,
-          'Authorization': "VWS #{Vufer.access_key}:#{signature}"
+          Date: time,
+          Authorization: "VWS #{Vufer.access_key}:#{signature}"
+        })
+
+        JSON.parse(res.body)
+      rescue StandardError => e
+        e.message
+      end
+
+      ##
+      # List all targets associated with server access keys and cloud database.
+      #
+      # @return [Array] A list of target ids associated with the account
+      def all
+        time = Time.now.httpdate
+        signature = Vufer::Signature.generate('/targets', nil, 'GET', time)
+
+        res = Faraday.get("#{Vufer::BASE_URI}/targets", {}, {
+          Date: time,
+          Authorization: "VWS #{Vufer.access_key}:#{signature}"
         })
 
         JSON.parse(res.body)
--- spec/vufer/target_spec.rb
+++ spec/vufer/target_spec.rb
@@ -40,4 +40,27 @@ RSpec.describe Vufer::Target do
       end
     end
   end
+
+  describe '#all' do
+    before do
+      Vufer.access_key = ENV['VWS_ACCESS_KEY']
+      Vufer.secret_key = ENV['VWS_SECRET_KEY']
+    end
+
+    it 'returns http status success' do
+      VCR.use_cassette('targets/all') do
+        target_ids = Vufer::Target.all
+
+        expect(target_ids['result_code']).to eq 'Success'
+      end
+    end
+
+    it 'the results is an array of ids' do
+      VCR.use_cassette('targets/all') do
+        target_ids = Vufer::Target.all
+
+        expect(target_ids['results']).to be_a Array
+      end
+    end
+  end
 end
