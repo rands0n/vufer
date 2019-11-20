f5114a02a27eb8d2699534c6ad1a11f2625eff0d
Randson <orandson@gmail.com>
2019-11-20 16:15:23 -0300

    create summary method to load info from Vuforia

--- lib/vufer.rb
+++ lib/vufer.rb
@@ -1,3 +1,5 @@
+require 'time'
+
 require 'vufer/version'
 require 'vufer/configure'
 require 'vufer/signature'
@@ -10,4 +12,24 @@ module Vufer
 
   class Error < StandardError; end
   class KeyEnvironmentError < Error; end
+
+  class << self
+    ##
+    # Returns a summary from targets, quota and images from Vuforia.
+    #
+    # @result [JSON] object containing all the info about targets, quota and images.
+    def summary
+      time = Time.now.httpdate
+      signature = Vufer::Signature.generate('/summary', nil, 'GET', time)
+
+      res = Faraday.get("#{Vufer::BASE_URI}/summary", {}, {
+        'Date': time,
+        'Authorization': "VWS #{Vufer.access_key}:#{signature}"
+      })
+
+      JSON.parse(res.body)
+    rescue StandardError => e
+      e.message
+    end
+  end
 end
--- spec/vufer_spec.rb
+++ spec/vufer_spec.rb
@@ -2,4 +2,35 @@ RSpec.describe Vufer do
   it 'has a version number' do
     expect(Vufer::VERSION).not_to be nil
   end
+
+  describe '#summary' do
+    before do
+      Vufer.configure do |config|
+        config.access_key = ENV['VWS_ACCESS_KEY']
+        config.secret_key = ENV['VWS_SECRET_KEY']
+      end
+    end
+
+    it 'returns http status code ok' do
+      VCR.use_cassette('summary') do
+        summary = Vufer.summary
+
+        expect(summary['result_code']).to eq 'Success'
+      end
+    end
+
+    %w[
+      active_images inactive_images failed_images processing_images
+      target_quota request_quota reco_threshold request_usage
+      total_recos current_month_recos previous_month_recos
+    ].each do |att|
+      it "includes attribute #{att}" do
+        VCR.use_cassette('summary') do
+          summary = Vufer.summary
+
+          expect(summary).to include att
+        end
+      end
+    end
+  end
 end
